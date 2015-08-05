e2f11436584c07d19ef78add0fa9d0458c054c11
carissamelanson <carissa.leos@gmail.com>
2015-08-04 22:25:06 -0500

    started working on connections.html

---
+++ final_project/star.js
@@ -0,0 +1,69 @@
+
+$(window).resize(function(){
+	if ($(window).width() <= 800){	
+		$('circle:nth-of-type(2)').attr({
+		  cx: '1000',
+		  cy: '500'
+		});
+
+		$('circle:nth-of-type(3)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(4)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(5)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(6)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(7)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(8)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(9)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(10)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(11)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(12)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(13)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+
+		$('circle:nth-of-type(14)').attr({
+		  cx: '800',
+		  cy: '600'
+		});
+	}	
+});
